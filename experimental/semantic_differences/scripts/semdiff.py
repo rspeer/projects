9032b6497d70f96fb0d85bb87c0f9d9f52126ef3
Elia Robyn Speer <elia@explosion.ai>
2021-09-01 22:16:26 +0000

    compare to the basic implementation that we know post-hoc

--- experimental/semantic_differences/scripts/semdiff.py
+++ experimental/semantic_differences/scripts/semdiff.py
@@ -22,13 +22,15 @@ class LabeledAttributeExample(BaseModel):
 
 
 # This function should be registered, but I don't know what it should be registered as
-def make_classifier(dropout: float=0.2, hidden_width: int=10) -> Model[Floats2d, Floats2d]:
+def make_classifier(dropout: float=0., hidden_width: int=2) -> Model[Floats2d, Floats2d]:
     """
     Make a simple neural classifier that takes in three concatenated word vectors
     and outputs a prediction from 0 to 1.
 
-    The input represents the vectors of two terms and an attribute that may or may not
-    distinguish them.
+    The input represents dot products of the term vectors:
+    - column 0: term1 @ attribute
+    - column 1: term2 @ attribute
+    - column 2: term1 @ term2
 
     The output should be near 1 if this is a discriminative attribute for the two terms
     (specifically, an attribute that relates to the first term and not the second),
@@ -36,7 +38,6 @@ def make_classifier(dropout: float=0.2, hidden_width: int=10) -> Model[Floats2d,
     """
     model: Model[Floats2d, Floats2d] = chain(
         Relu(nO=hidden_width, dropout=dropout),
-        Relu(nI=hidden_width, nO=hidden_width, dropout=dropout),
         Relu(nI=hidden_width, nO=1),
         Sigmoid(),
     )
@@ -80,8 +81,9 @@ def examples_to_arrays(examples: Iterable[LabeledAttributeExample]) -> Tuple[Flo
     """
     Convert a list of DiscriminAtt examples to input and output arrays.
 
-    The rows of the input consist of concatenated 300d vectors of the three terms. The
-    output is an Nx1 matrix whose entries are 1 or 0 for positive or negative examples.
+    The rows of the input consist of three dot products, representing the dot product of each
+    pair of vectors from (term1, term2, attribute). The output is an Nx1 matrix whose entries
+    are 1 or 0 for positive or negative examples.
     """
     def vector_pipe(texts: List[str]) -> np.ndarray:
         disabled_features = ["tagger", "parser", "ner", "textcat", "lemmatizer"]
@@ -96,7 +98,16 @@ def examples_to_arrays(examples: Iterable[LabeledAttributeExample]) -> Tuple[Flo
     vectors1 = vector_pipe(terms1)
     vectors2 = vector_pipe(terms2)
     att_vectors = vector_pipe(attributes)
-    input_array = cast(Floats2d, np.hstack([vectors1, vectors2, att_vectors]))
+
+    # I'm testing right now how to learn a very, very simple decision function, and all it needs
+    # is the dot products of each term vector with the attribute vector.
+    dots1 = (vectors1 * att_vectors).sum(axis=1)[:, np.newaxis]
+    dots2 = (vectors2 * att_vectors).sum(axis=1)[:, np.newaxis]
+
+    # In case it helps as a comparison, also include the dot products of the terms with each other.
+    dots3 = (vectors1 * vectors2).sum(axis=1)[:, np.newaxis]
+
+    input_array = cast(Floats2d, np.hstack([dots1, dots2, dots3]))
     output_array = cast(Floats2d, np.array(outputs))
     return input_array, output_array
 
@@ -117,7 +128,7 @@ def train_model(model, optimizer, n_iter, batch_size):
             model.finish_update(optimizer)
         
         score = evaluate(model, val_X, val_Y, batch_size)
-        print(f"{n}\t{loss:.2f}\t{score:.3f}")
+        print(f"{n}\t{loss:.5f}\t{score:.3f}")
     
 
 def evaluate(model, val_X, val_Y, batch_size):
@@ -130,11 +141,31 @@ def evaluate(model, val_X, val_Y, batch_size):
     return correct / total
 
 
+def evaluate_basic(val_X, val_Y):
+    """
+    Implement a high-performing strategy from SemEval-2018: just compare the dot products
+    of the two terms with the attribute.
+
+    This should be an extremely easy function to learn, and on SpaCy's vectors, it gets
+    an accuracy of 62.9%. (On vectors retrofit with ConceptNet, it got 73.6%.)
+    """
+    yh = (val_X[:, 0] - val_X[:, 1]) > 0.05
+    y = val_Y.flatten()
+    correct = ((yh > 0.5) == y).sum()
+    total = len(yh)
+    return correct / total
+
+
 def run():
+    # First, show the result that we can get by already knowing the best simple solution
+    val_X, val_Y = examples_to_arrays(read_discriminatt_file("assets/DiscriminAtt/training/validation.txt"))
+    print(evaluate_basic(val_X, val_Y))
+
+    # Now try to train a machine learning model
     model = make_classifier()
     optimizer = Adam(0.001)
-    n_iter = 100
-    batch_size = 128
+    n_iter = 50
+    batch_size = 32
     train_model(model, optimizer, n_iter, batch_size)
 
 
